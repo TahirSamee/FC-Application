300e9aa3ad27a88ec02365ea5fa55485666c1c5f
Tahir Samee <72864292+TahirSamee@users.noreply.github.com>
2025-08-01 18:38:56 +0500

    Added New Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FC_Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
