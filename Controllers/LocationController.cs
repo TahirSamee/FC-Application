35332910a487e7900bee1c0793aee4bac9dabd00
Tahir Samee <72864292+TahirSamee@users.noreply.github.com>
2025-08-01 19:10:22 +0500

    Added Location

---
+++ Controllers/LocationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FC_Application.Controllers
+{
+    public class LocationController : Controller
+    {
+        public IActionResult Location()
+        {
+            return View();
+        }
+    }
+}
